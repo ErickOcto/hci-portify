f45eb70b8e63b355030cf44b684f7705048f5f51
Rey <95080700+Rey0614@users.noreply.github.com>
2026-06-17 19:14:12 +0700

    Add landing, signup, login pages and Live Preview feature

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        landing: resolve(__dirname, 'src/pages/landing.html'),
+        signup: resolve(__dirname, 'src/pages/signup.html'),
+        login: resolve(__dirname, 'src/pages/login.html'),
         uploadcv: resolve(__dirname, 'src/pages/upload-cv.html'),
         aianalysis: resolve(__dirname, 'src/pages/ai-analysis.html'),
         design: resolve(__dirname, 'src/pages/design-selection.html'),

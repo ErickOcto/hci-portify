c78f5891dc18b0fecfc27c249a565d5091e0ab48
rbbieee <211218120+rbbieee@users.noreply.github.com>
2026-06-17 15:32:35 +0700

    feat: add creating flow pages + update vite config + update index

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        uploadcv: resolve(__dirname, 'src/pages/upload-cv.html'),
+        aianalysis: resolve(__dirname, 'src/pages/ai-analysis.html'),
+        design: resolve(__dirname, 'src/pages/design-selection.html'),
+        publish: resolve(__dirname, 'src/pages/publish.html'),
+      }
+    }
+  }
 })
